08807d1657747e77fe457345cc7b25f4ff41790d
Brenton <brentonasaunders@gmail.com>
2022-11-13 18:13:10 -0500

    Modified MainView to be extended by LobbyView

--- src/views/MainView.php
+++ src/views/MainView.php
@@ -8,43 +8,63 @@ class MainView implements View {
         $this->gameId = $gameId;
     }
 
-    public function render() {
+    protected function head() {
         $gameId = ($this->gameId) ? "\"{$this->gameId}\"" : "null";
 
-        echo <<<EOD
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Say Anything</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="css/index.css">
-        <script src="js/jquery-3.6.1.min.js"></script>
-        <script>
-            const GAME_ID = $gameId;
-        </script>
-        <script src="js/index.js"></script>
-    </head>
-    <body>
-        <div id="app">
-            <header>
-                <div class="left">
-                    <div id="menu">
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                    </div>
-                </div>
-                <div class="center"><div id="title">Say Anything</div></div>
-                <div class="right"></div>
-            </header>
-            <main>
-                
-            </main>
-        </div>
-    </body>
-</html>
-        
-EOD;
+        echo "<title>Say Anything</title>";
+        echo '<meta charset="UTF-8">';
+        echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
+        echo '<link rel="stylesheet" href="css/index.css">';
+        echo '<script src="js/jquery-3.6.1.min.js"></script>';
+        echo "<script> const GAME_ID = $gameId; </script>";
+        echo '<script src="js/index.js"></script>';
+    }
+
+    protected function body() {
+        echo '<div id="app">';
+        echo "<header>";
+
+        $this->header();
+
+        echo "</header>";
+
+        echo "<main>";
+
+        $this->main();
+
+        echo "</main>";
+        echo "</div>";
+    }
+
+    protected function header() {
+        echo '<div class="left">';
+        echo '<div id="menu">';
+        echo "<div></div>";
+        echo "<div></div>";
+        echo "<div></div>";
+        echo "</div>";
+        echo "</div>";
+        echo '<div class="center"><div id="title">Say Anything</div></div>';
+        echo '<div class="right"></div>';
+    }
+
+    protected function main() {
+
+    }
+
+    public function render() {
+        echo "<!DOCTYPE html>";
+        echo "<html>";
+        echo "<head>";
+
+        $this->head();
+
+        echo "</head>";
+        echo "<body>";
+
+        $this->body();
+
+        echo "</body>";
+        echo "</html>";
     }
 }
