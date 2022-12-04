466d443d444a5e657f2d83f67db058e026905d99
Brenton <brentonasaunders@gmail.com>
2022-12-04 16:31:34 -0500

    Commit

--- src/views/ScoreBoardView.php
+++ src/views/ScoreBoardView.php
@@ -15,49 +15,49 @@ class ScoreBoardView implements View {
     }
 
     public function render() {
-        echo '<div id="game">';
-        echo '<div class="head">';
-        echo "</div>";
-        echo '<div class="body">';
         echo '<div id="score-board">';
-
-        echo '<div class="table">';
 
         echo '<div class="caption">Score for Each Round</div>';
 
         echo '<div class="head">';
-        echo '<div class="row">';
 
-        for($round = 1; $round <= 12; ++$round) {
-            echo "<div class=\"col\">$round</div>";
+        for ($i = 1; $i <= 12; ++$i) {
+            echo "<div class=\"col\">$i</div>";
         }
 
-        echo '<div class="col"><span>Total</span></div>';
-        echo "</div>";
-        echo "</div>";
+        echo '<div class="col">Total</div>';
+
+        echo '</div>';
 
         echo '<div class="body">';
 
-        $tokens = Player::getTokens();
+        $tokens = [
+            "guitar",
+            "dollar-sign",
+            "high-heels",
+            "computer",
+            "football",
+            "martini-glass",
+            "clapperboard",
+            "car"
+        ];
 
-        foreach($tokens as $token) {
+        foreach ($tokens as $token) {
             echo "<div class=\"row $token\">";
-            echo "<div class=\"col\"></div>";
 
-            for($round = 1; $round <= 12; ++$round) {
-                echo "<div class=\"col\"><span>0</span></div>";
+            echo '<div class="col"></div>';
+
+            for ($i = 1; $i <= 12; ++$i) {
+                echo '<div class="col"><span>0</span></div>';
             }
 
             echo '<div class="col"><span>0</span></div>';
+
             echo "</div>";
         }
 
         echo "</div>";
 
-        echo "</div>";
-
-        echo "</div>";
-        echo "</div>";
         echo "</div>";
     }
 }
